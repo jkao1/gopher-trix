aba4ca5b1e9e143209dc173ea5791855ab740272
Jason Kao <jasonkao85@gmail.com>
2018-02-25 13:32:08 -0500

    added buffer write to ppm

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 var screen [][][]int
@@ -9,4 +10,20 @@ var screen [][][]int
 func main() {
 	screen = NewScreen()
 	fmt.Println(screen)
+}
+
+func WriteScreenToPPM() {
+	file, err := os.OpenFile("foo.ppm", os.O_CREATE | os.O_WRONLY, 0644)
+
+	if (err != nil) {
+		panic(err)
+	}
+
+	var buffer bytes.Buffer
+
+	buffer.WriteString("P3 %d %d 255\n", XRES, YRES)
+	// numerous string joins
+
+	// Write buffer to file
+	file.WriteString(buffer.String())
 }
