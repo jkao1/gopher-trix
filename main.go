e74e1bd4ceaa8cbb918ebc7e6a96aa1f9fff7d58
Jason Kao <jasonkao85@gmail.com>
2018-02-25 19:17:22 -0500

    write buffer works

--- main.go
+++ main.go
@@ -8,11 +8,13 @@ import (
 
 func main() {
 	screen := NewScreen()
+
+	DrawLine(screen, 0, 0, XRES - 1, YRES - 1)
+
 	WriteScreenToPPM(screen)
 }
 
 func WriteScreenToPPM(screen [][][]int) {
-	return
 	file, err := os.OpenFile("foo.ppm", os.O_CREATE | os.O_WRONLY, 0644)
 
 	if (err != nil) {
@@ -22,10 +24,13 @@ func WriteScreenToPPM(screen [][][]int) {
 	defer file.Close()
 
 	var buffer bytes.Buffer
-
 	buffer.WriteString(fmt.Sprintf("P3 %d %d 255\n", XRES, YRES))
-	// numerous string joins
+	for i := 0; i < YRES; i++ {
+		for j := 0; j < XRES; j++ {
+			rgb := screen[i][j]
+			buffer.WriteString(fmt.Sprintf("%d %d %d ", uint8(rgb[0]), uint8(rgb[1]), uint8(rgb[2])))
+		}
+	}
 
-	// Write buffer to file
 	file.WriteString(buffer.String())
 }
